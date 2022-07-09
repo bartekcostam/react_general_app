dc68ca309792c6da5a18f7797b2f9efb54a93adc
Bartlomiej Wydmuch <bartekoffice1@gmail.com>
2022-07-09 21:58:42 +0200

    quick set for axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
+import axios from 'axios'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component {
+
+
+
+  componentDidMount() {
+
+    const data = axios.get("https://dummy.restapiexample.com/api/v1/employees",null).then(response => {
+      console.log(response);
+    })
+    
+  }
+
+
+
+  render(){
+
+
+
+
+    return (
+      <div className="App">
+        <h1>Costam</h1>
+        
+      </div>
+    )
+  
+
+    }
+
+    }
+
+
 
 export default App;
